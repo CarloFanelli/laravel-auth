ab80da4dd45decc1c5124dbad5d085527f6293a1
CarloFanelli <carlo.fanelli.sax@gmail.com>
2023-11-08 16:15:22 +0100

    add project migration, model, seeder and admin dashboard template

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -2,16 +2,29 @@
 
 namespace Database\Seeders;
 
+use App\Models\Project;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+use Faker\Generator as Faker;
 
 class ProjectSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run(Faker $faker): void
     {
-        //
+        for ($i = 0; $i < 10; $i++) {
+
+            $project = new Project();
+
+            $project->title = $faker->realText(15);
+            $project->content = $faker->realText();
+            $project->slug = Str::slug($project->title, '-');
+            $project->cover_image = $faker->image();
+
+            $project->save();
+        }
     }
 }
